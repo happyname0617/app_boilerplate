8def1e6bc88be04653a1bf79a5583e613ac8cfa1
Andre Sattler <andresattler.webdev@gmail.com>
2018-12-18 22:34:40 +0100

    added css-modules and more a mockup

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
-import * as React from 'react'
+import React from 'react'
 
+import styles from './styles.scss'
 
 const App = () => {
 
   return (
-      <div>
+      <div className={styles.container}>
         <h1>Similar Food</h1>
+        <img className={styles.image} src='https://c2.staticflickr.com/4/3092/2403194442_c537654716_z.jpg?zz=1' />
+        <img className={styles.image} src='https://c2.staticflickr.com/4/3092/2403194442_c537654716_z.jpg?zz=1' />
       </div>
   )
 }
---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(tsx?$)/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]-[local]-[hash:base64:6]',
+              camelCase: true
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js', 'scss' ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+}
