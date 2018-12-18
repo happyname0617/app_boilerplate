d0d9cf8f58db10d3e3f3f44169d51cfa0cebc85e
Andre Sattler <andresattler.webdev@gmail.com>
2018-12-18 21:53:15 +0100

    removed previos test code

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,10 @@ import * as React from 'react'
 
 
 const App = () => {
-  const [count, setCount] = React.useState(0)
-  const handleClick = React.useCallback(() => setCount(count + 1), [count])
 
   return (
       <div>
-        <button onClick={handleClick}> Plus 1 </button>
-        <h1>Hello World</h1>
-        <h1>{count}</h1>
+        <h1>Similar Food</h1>
       </div>
   )
 }
