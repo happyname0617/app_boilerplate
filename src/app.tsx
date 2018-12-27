f004f7bf722ff5646705bd52a63ba41c3cc5f042
Andre Sattler <andresattler.webdev@gmail.com>
2018-12-27 22:58:06 +0100

    basic hover-effect

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Card from './components/card/card'
+
 import styles from './styles.scss'
 
 const App = () => {
@@ -7,8 +9,8 @@ const App = () => {
   return (
       <div className={styles.container}>
         <h1>Similar Food</h1>
-        <img className={styles.image} src='https://c2.staticflickr.com/4/3092/2403194442_c537654716_z.jpg?zz=1' />
-        <img className={styles.image} src='https://c2.staticflickr.com/4/3092/2403194442_c537654716_z.jpg?zz=1' />
+        <Card />
+        <Card />
       </div>
   )
 }
