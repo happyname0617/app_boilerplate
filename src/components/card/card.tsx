e78f14bfa2d19661a2bf8c4ba1b2e200a7f5e5e4
Andre Sattler <andresattler.webdev@gmail.com>
2018-12-29 19:38:32 +0100

    changed hover-effect to onClick

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from 'react'
+import cx from 'classnames';
+import React, { useState } from 'react'
 
 import styles from './styles.scss'
 
-const Overlay = () => {
+const Overlay = ({ open }: { open: boolean }) => {
   return (
     <figcaption className={styles.overlay}>
       <h3 className={styles.title}>Food Name</h3>
@@ -12,10 +13,11 @@ const Overlay = () => {
 }
 
 const Card = ({ url }: { url: string }) => {
+  const [ open, setOpen ] = useState(false)
   return (
-    <figure className={styles.wrapper}>
+    <figure onClick={() => setOpen(!open)} className={cx(styles.wrapper, open && styles.open)}>
       <img  className={styles.image } src={url} />
-      <Overlay />
+      <Overlay open={ open }/>
     </figure>
   )
 }
