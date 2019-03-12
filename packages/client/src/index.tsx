e86d59c0bad259a4e4523340c02e7c33014acc65
Andre Sattler <andresattler.webdev@gmail.com>
2019-03-12 21:41:07 +0100

    added service-worker and basic offline support

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
+// @ts-ignore
+import swRuntime from 'serviceworker-webpack-plugin/lib/runtime';
 
 import './iconfont'
 import App from './app'
+
+
+if ('serviceWorker' in navigator) {
+  swRuntime.register();
+}
 
 const root = document.getElementById('root')
 
